2a0ca3ea5bbcdc82596f344fd6b547a74f2a28fd
Jasvinder Singh <jasvinder15atech@gmail.com>
2020-09-21 23:01:44 +0530

    added assy-await get diagram and reacreated the boilerplate for asyn-await get request

--- public/main.js
+++ public/main.js
@@ -1,40 +1,13 @@
-// Information to reach API
-const url = "https://api.datamuse.com/words?";
-const queryParams = "rel_jjb=";
-const additionalParams = "&topics=";
-
-// Selecting page elements
-const inputField = document.querySelector("#input");
-const topicField = document.querySelector("#topic");
-const submit = document.querySelector("#submit");
-const responseField = document.querySelector("#responseField");
-
-// AJAX function
-const getSuggestions = () => {
-  const wordQuery = inputField.value;
-  const topicQuery = topicField.value;
-  const endpoint = `${url}${queryParams}${wordQuery}${additionalParams}${topicQuery}`;
-
-  const xhr = new XMLHttpRequest();
-  xhr.responseType = "json";
-
-  xhr.onreadystatechange = () => {
-    if (xhr.readyState === XMLHttpRequest.DONE) {
-      renderResponse(xhr.response);
+const getData = async () => {
+  try {
+    const response = await fetch('https://api-to-call.com/endpoint')
+    if(response.ok){
+      const jsonResponse = await response.json();
+      return jsonResponse
     }
-  };
-
-  xhr.open("GET", endpoint);
-  xhr.send();
-};
-
-// Clear previous results and display results to webpage
-const displaySuggestions = (event) => {
-  event.preventDefault();
-  while (responseField.firstChild) {
-    responseField.removeChild(responseField.firstChild);
+    throw new Error('Request failed!') 
   }
-  getSuggestions();
-};
-
-submit.addEventListener("click", displaySuggestions);
+  catch(error){
+    console.log(error)
+  }
+}
